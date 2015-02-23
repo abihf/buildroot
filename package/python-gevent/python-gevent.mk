675e21cb0d452c3deff303b986457876f21aaa8f
Abi Hafshin <abi@hafs.in>
2015-02-23 07:25:31 +0000

    fix dependency error on python-gevent and python-greenlet

--- package/python-gevent/python-gevent.mk
+++ package/python-gevent/python-gevent.mk
@@ -9,7 +9,9 @@ PYTHON_GEVENT_SOURCE = gevent-$(PYTHON_GEVENT_VERSION).tar.gz
 PYTHON_GEVENT_SITE = https://pypi.python.org/packages/source/g/gevent/
 PYTHON_GEVENT_LICENSE = MIT
 PYTHON_GEVENT_LICENSE_FILES = LICENSE
-PYTHON_GEVENT_SETUP_TYPE = distutils
-PYTHON_GEVENT_DEPENDENCIES = python-greenlet
+PYTHON_GEVENT_SETUP_TYPE = setuptools
+PYTHON_GEVENT_BASE_BUILD_TGT = build
+PYTHON_GEVENT_ENV = EMBED=0
+PYTHON_GEVENT_DEPENDENCIES = python-greenlet libev c-ares
 
 $(eval $(python-package))
--- package/python-greenlet/python-greenlet.mk
+++ package/python-greenlet/python-greenlet.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 PYTHON_GREENLET_VERSION = 0.4.5
-PYTHON_GREENLET_SOURCE = greenlet-$(PYTHON_GREENLET_VERSION).tar.gz
-PYTHON_GREENLET_SITE = https://pypi.python.org/packages/source/g/greenlet/
+PYTHON_GREENLET_SOURCE = $(PYTHON_GREENLET_VERSION).tar.gz
+PYTHON_GREENLET_SITE = https://github.com/python-greenlet/greenlet/archive/
 PYTHON_GREENLET_LICENSE = MIT
 PYTHON_GREENLET_LICENSE_FILES = LICENSE
-PYTHON_GREENLET_SETUP_TYPE = setuptool
-PYTHON_GREENLET_DEPENDENCIES = libev
+PYTHON_GREENLET_SETUP_TYPE = distutils
+PYTHON_GREENLET_DEPENDENCIES = 
 
 $(eval $(python-package))
